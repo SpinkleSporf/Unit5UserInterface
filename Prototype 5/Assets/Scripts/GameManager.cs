e95e89a6d79e0ca3c25492058d4e6a9345953675
Matt Brennan <mattbrennan1123@gmail.com>
2023-05-05 11:18:35 -0400

    5.3 done

--- Prototype 5/Assets/Scripts/GameManager.cs
+++ Prototype 5/Assets/Scripts/GameManager.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -9,17 +11,31 @@ public class GameManager : MonoBehaviour
     private float spawnRate = 1.0f;
     private int score;
     public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+    public bool isGameActive;
+    public Button restartButton;
     // Start is called before the first frame update
     void Start() 
     {
+        isGameActive= true;
         StartCoroutine(SpawnTarget());
         score = 0;
         UpdateScore(0);
     }
 
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    public void GameOver()
+    {
+        gameOverText.gameObject.SetActive(true);
+        isGameActive= false;
+        restartButton.gameObject.SetActive(true);
+    }
     IEnumerator SpawnTarget()
     {
-        while (true)
+        while (isGameActive)
         {
             yield return new WaitForSeconds(spawnRate);
             int index = Random.Range(0, targets.Count);
